45cf231210059fb4495325b7966792698571da80
Anton Bakai <inso@i.ua>
2018-03-13 13:44:04 +0200

    added docblocks

--- src/State/State.php
+++ src/State/State.php
@@ -4,11 +4,38 @@ namespace VisualCraft\Logmon\State;
 
 class State
 {
+    /**
+     * @var int
+     */
     public $offset;
+
+    /**
+     * @var string
+     */
     public $startSign;
+
+    /**
+     * @var int
+     */
     public $startSignOffset1;
+
+    /**
+     * @var int
+     */
     public $startSignOffset2;
+
+    /**
+     * @var string
+     */
     public $endSign;
+
+    /**
+     * @var int
+     */
     public $endSignOffset1;
+
+    /**
+     * @var int
+     */
     public $endSignOffset2;
 }
